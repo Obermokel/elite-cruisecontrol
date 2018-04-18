97a203852653126eaff63103b24de5fbc1276f3c
Boris Günther <boris.guenther@redteclab.com>
2018-04-18 15:20:12 +0200

    Code cleanup

--- src/main/java/borg/ed/cruisecontrol/GameState.java
+++ src/main/java/borg/ed/cruisecontrol/GameState.java
@@ -2,60 +2,51 @@ package borg.ed.cruisecontrol;
 
 public enum GameState {
 
-	UNKNOWN,
+    UNKNOWN,
 
-	/**
-	 * Next system ahead, charging but not in jump yet, so keep aligning to the next system.
-	 */
-	FSD_CHARGING,
+    /**
+     * Next system ahead, charging but not in jump yet, so keep aligning to the next system.
+     */
+    FSD_CHARGING,
 
-	/**
-	 * As soon as the FSD jump countdown starts we are in hyperspace. Throttle to 0%, then wait until arrived.
-	 */
-	IN_HYPERSPACE,
+    /**
+     * As soon as the FSD jump countdown starts we are in hyperspace. Throttle to 0%, then wait until arrived.
+     */
+    IN_HYPERSPACE,
 
-	/**
-	 * Honk and wait until bodies discovered.
-	 */
-	HONKING,
+    /**
+     * Honk and wait until bodies discovered.
+     */
+    HONKING,
 
-	/**
-	 * Accelerate to 25% and wait until we are scooping fuel.
-	 */
-	GET_IN_SCOOPING_RANGE,
+    /**
+     * Accelerate to 25% and wait until we are scooping fuel.
+     */
+    GET_IN_SCOOPING_RANGE,
 
-	/**
-	 * Throttle to 0% and wait until full.
-	 */
-	SCOOPING_FUEL,
+    /**
+     * Throttle to 0% and wait until full.
+     */
+    SCOOPING_FUEL,
 
-	/**
-	 * Turn nose down until sight is clear.
-	 */
-	ALIGN_TO_STAR_ESCAPE,
+    /**
+     * Turn nose down until sight is clear.
+     */
+    ALIGN_TO_STAR_ESCAPE,
 
-	/**
-	 * At 25% throttle escape, again turning the nose down if there is too much brightness ahead. Do until not in fuel scoop range any more.
-	 */
-	ESCAPE_FROM_STAR_SLOW,
+    /**
+     * At 25% throttle escape, again turning the nose down if there is too much brightness ahead. Do until not in fuel scoop range any more.
+     */
+    ESCAPE_FROM_STAR_SLOW,
 
-	/**
-	 * At 75% throttle escape for 10 seconds, again turning the nose down if there is too much brightness ahead.
-	 */
-	ESCAPE_FROM_STAR_FASTER,
+    /**
+     * At 75% throttle escape for 10 seconds, again turning the nose down if there is too much brightness ahead.
+     */
+    ESCAPE_FROM_STAR_FASTER,
 
-	/**
-	 * At 100% throttle align to the next system.
-	 */
-	ALIGN_TO_NEXT_SYSTEM,
-
-	COMPASS_ALIGNING_JUMP,
-	COMPASS_ALIGNED_JUMP,
-	JUMPING,
-	HONK_AND_SELECT,
-	COMPASS_ALIGNING_STAR,
-	COMPASS_ALIGNED_STAR,
-	COMPASS_ALIGNING_ESCAPE,
-	COMPASS_ALIGNED_ESCAPE;
+    /**
+     * At 100% throttle align to the next system.
+     */
+    ALIGN_TO_NEXT_SYSTEM;
 
 }
