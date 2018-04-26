23b6ba47633566ef32971da6ff62a65a9fb49b57
Boris Günther <boris.guenther@redteclab.com>
2018-04-26 17:51:51 +0200

    Fixed MouseUtil

--- src/main/java/borg/ed/cruisecontrol/util/MouseUtil.java
+++ src/main/java/borg/ed/cruisecontrol/util/MouseUtil.java
@@ -4,79 +4,62 @@ import java.awt.Point;
 
 public class MouseUtil {
 
-	private final int screenWidth;
-	private final int screenHeight;
-	private final int imageWidth;
-	private final int imageHeight;
+    private final int screenWidth;
+    private final int screenHeight;
+    private final int imageWidth;
+    private final int imageHeight;
 
-	private final float scale;
-	private final int paddingLeftRight;
-	private final int paddingTopBottom;
+    private final float scale;
+    private final int paddingLeftRight;
+    private final int paddingTopBottom;
 
-	public MouseUtil(int screenWidth, int screenHeight, int imageWidth, int imageHeight) {
-		if (screenWidth < imageWidth) {
-			throw new IllegalArgumentException("Screen width " + screenWidth + " must not be less than image width " + imageWidth);
-		} else if (screenHeight < imageHeight) {
-			throw new IllegalArgumentException("Screen height " + screenHeight + " must not be less than image height " + imageHeight);
-		} else {
-			this.screenWidth = screenWidth;
-			this.screenHeight = screenHeight;
-			this.imageWidth = imageWidth;
-			this.imageHeight = imageHeight;
+    public MouseUtil(int screenWidth, int screenHeight, int imageWidth, int imageHeight) {
+        if (screenWidth < imageWidth) {
+            throw new IllegalArgumentException("Screen width " + screenWidth + " must not be less than image width " + imageWidth);
+        } else if (screenHeight < imageHeight) {
+            throw new IllegalArgumentException("Screen height " + screenHeight + " must not be less than image height " + imageHeight);
+        } else {
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+            this.imageWidth = imageWidth;
+            this.imageHeight = imageHeight;
 
-			float scaleHeight = (float) imageHeight / (float) screenHeight;
-			int scaledWidth = Math.round(scaleHeight * screenWidth);
+            float scale = (float) screenHeight / (float) imageHeight;
+            int scaledWidth = Math.round(imageWidth * scale);
+            int scaledHeight = Math.round(imageHeight * scale);
+            int paddingLeftRight = (screenWidth - scaledWidth) / 2;
+            int paddingTopBottom = (screenHeight - scaledHeight) / 2;
+            if (paddingLeftRight < 0 || paddingTopBottom < 0) {
+                scale = (float) screenWidth / (float) imageWidth;
+                scaledWidth = Math.round(imageWidth * scale);
+                scaledHeight = Math.round(imageHeight * scale);
+                paddingLeftRight = (screenWidth - scaledWidth) / 2;
+                paddingTopBottom = (screenHeight - scaledHeight) / 2;
+            }
+            this.scale = scale;
+            this.paddingLeftRight = paddingLeftRight;
+            this.paddingTopBottom = paddingTopBottom;
+        }
+    }
 
-			if (scaledWidth == imageWidth) {
-				// Perfect!
-				this.scale = scaleHeight;
-				this.paddingLeftRight = 0;
-				this.paddingTopBottom = 0;
-			} else if (scaledWidth > imageWidth) {
-				// Left and right can be cropped
-				this.scale = scaleHeight;
-				this.paddingLeftRight = (screenWidth - scaledWidth) / 2;
-				this.paddingTopBottom = 0;
-			} else {
-				// Width would be too small, scale by width instead
-				float scaleWidth = (float) imageWidth / (float) scaledWidth;
-				int scaledHeight = Math.round(scaleWidth * screenHeight);
+    public Point imageToScreen(Point pImage) {
+        return new Point(Math.round(pImage.x / this.scale) + this.paddingLeftRight, Math.round(pImage.y / this.scale) + this.paddingTopBottom);
+    }
 
-				if (scaledHeight == imageHeight) {
-					// Perfect!
-					this.scale = scaleWidth;
-					this.paddingLeftRight = 0;
-					this.paddingTopBottom = 0;
-				} else if (scaledHeight > imageHeight) {
-					// Top and bottom can be cropped
-					this.scale = scaleWidth;
-					this.paddingLeftRight = 0;
-					this.paddingTopBottom = (screenHeight - scaledHeight) / 2;
-				} else {
-					throw new IllegalStateException(scaledWidth + "x" + screenHeight + " -> " + imageWidth + "x" + imageHeight);
-				}
-			}
-		}
-	}
+    public int getScreenWidth() {
+        return screenWidth;
+    }
 
-	public Point imageToScreen(Point pImage) {
-		return new Point(Math.round(pImage.x / this.scale) + this.paddingLeftRight, Math.round(pImage.y / this.scale) + this.paddingTopBottom);
-	}
+    public int getScreenHeight() {
+        return screenHeight;
+    }
 
-	public int getScreenWidth() {
-		return screenWidth;
-	}
+    public int getImageWidth() {
+        return imageWidth;
+    }
 
-	public int getScreenHeight() {
-		return screenHeight;
-	}
-
-	public int getImageWidth() {
-		return imageWidth;
-	}
-
-	public int getImageHeight() {
-		return imageHeight;
-	}
+    public int getImageHeight() {
+        return imageHeight;
+    }
 
 }
