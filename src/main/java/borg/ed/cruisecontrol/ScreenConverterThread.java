5a8358ecf8fca278670dd9d1ed846cc9b37c0b5c
Boris Günther <boris.guenther@googlemail.com>
2018-04-26 22:23:54 +0200

    WIP

--- src/main/java/borg/ed/cruisecontrol/ScreenConverterThread.java
+++ src/main/java/borg/ed/cruisecontrol/ScreenConverterThread.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import boofcv.alg.color.ColorHsv;
+import boofcv.alg.filter.blur.GBlurImageOps;
 import boofcv.io.image.ConvertBufferedImage;
 import boofcv.struct.image.GrayF32;
 import boofcv.struct.image.Planar;
@@ -11,111 +12,112 @@ import borg.ed.cruisecontrol.util.ImageUtil;
 
 public class ScreenConverterThread extends Thread {
 
-    static final Logger logger = LoggerFactory.getLogger(ScreenConverterThread.class);
+	static final Logger logger = LoggerFactory.getLogger(ScreenConverterThread.class);
 
-    public volatile boolean shutdown = false;
+	public volatile boolean shutdown = false;
 
-    private final ScreenReaderResult screenReaderResult;
-    private final ScreenConverterResult screenConverterResult;
+	private final ScreenReaderResult screenReaderResult;
+	private final ScreenConverterResult screenConverterResult;
 
-    public ScreenConverterThread(ScreenReaderResult screenReaderResult, ScreenConverterResult screenConverterResult) {
-        this.setName("SCThread");
-        this.setDaemon(true);
+	public ScreenConverterThread(ScreenReaderResult screenReaderResult, ScreenConverterResult screenConverterResult) {
+		this.setName("SCThread");
+		this.setDaemon(true);
 
-        this.screenReaderResult = screenReaderResult;
-        this.screenConverterResult = screenConverterResult;
-    }
+		this.screenReaderResult = screenReaderResult;
+		this.screenConverterResult = screenConverterResult;
+	}
 
-    @Override
-    public void run() {
-        logger.info(this.getName() + " started");
+	@Override
+	public void run() {
+		logger.info(this.getName() + " started");
 
-        Planar<GrayF32> rgb = new Planar<>(GrayF32.class, CruiseControlApplication.SCALED_WIDTH, CruiseControlApplication.SCALED_HEIGHT, 3);
-        Planar<GrayF32> hsv = rgb.createSameShape();
+		Planar<GrayF32> rgb = new Planar<>(GrayF32.class, CruiseControlApplication.SCALED_WIDTH, CruiseControlApplication.SCALED_HEIGHT, 3);
+		Planar<GrayF32> hsv = rgb.createSameShape();
 
-        GrayF32 orangeHudImage = new GrayF32(CruiseControlApplication.SCALED_WIDTH, CruiseControlApplication.SCALED_HEIGHT);
-        GrayF32 yellowHudImage = orangeHudImage.createSameShape();
-        GrayF32 blueWhiteHudImage = orangeHudImage.createSameShape();
-        GrayF32 redHudImage = orangeHudImage.createSameShape();
-        GrayF32 brightImage = orangeHudImage.createSameShape();
+		GrayF32 orangeHudImage = new GrayF32(CruiseControlApplication.SCALED_WIDTH, CruiseControlApplication.SCALED_HEIGHT);
+		GrayF32 yellowHudImage = orangeHudImage.createSameShape();
+		GrayF32 blueWhiteHudImage = orangeHudImage.createSameShape();
+		GrayF32 redHudImage = orangeHudImage.createSameShape();
+		GrayF32 brightImage = orangeHudImage.createSameShape();
 
-        while (!Thread.currentThread().isInterrupted() && !this.shutdown) {
-            // Wait for the next (scaled) screen capture and convert it into BoofCV format
-            synchronized (this.screenReaderResult) {
-                try {
-                    this.screenReaderResult.wait();
-                    ConvertBufferedImage.convertFromMulti(this.screenReaderResult.getScaledScreenCapture(), rgb, true, GrayF32.class);
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
+		while (!Thread.currentThread().isInterrupted() && !this.shutdown) {
+			// Wait for the next (scaled) screen capture and convert it into BoofCV format
+			synchronized (this.screenReaderResult) {
+				try {
+					this.screenReaderResult.wait();
+					ConvertBufferedImage.convertFromMulti(this.screenReaderResult.getScaledScreenCapture(), rgb, true, GrayF32.class);
+				} catch (InterruptedException e) {
+					break;
+				}
+			}
 
-            // Convert into relevant color bands
-            rgb = ImageUtil.normalize255(rgb);
-            ColorHsv.rgbToHsv_F32(rgb, hsv);
-            this.hsvToHudImages(hsv, orangeHudImage, yellowHudImage, blueWhiteHudImage, redHudImage, brightImage);
+			// Convert into relevant color bands
+			rgb = ImageUtil.normalize255(rgb);
+			ColorHsv.rgbToHsv_F32(rgb, hsv);
+			this.hsvToHudImages(hsv, orangeHudImage, yellowHudImage, blueWhiteHudImage, redHudImage, brightImage);
+			yellowHudImage = GBlurImageOps.gaussian(yellowHudImage, null, -1, 1, null);
 
-            // Notify waiting threads
-            synchronized (this.screenConverterResult) {
-                this.screenConverterResult.setRgb(rgb);
-                this.screenConverterResult.setHsv(hsv);
-                this.screenConverterResult.setOrangeHudImage(orangeHudImage);
-                this.screenConverterResult.setYellowHudImage(yellowHudImage);
-                this.screenConverterResult.setBlueWhiteHudImage(blueWhiteHudImage);
-                this.screenConverterResult.setRedHudImage(redHudImage);
-                this.screenConverterResult.setBrightImage(brightImage);
-                this.screenConverterResult.notifyAll();
-            }
-        }
+			// Notify waiting threads
+			synchronized (this.screenConverterResult) {
+				this.screenConverterResult.setRgb(rgb);
+				this.screenConverterResult.setHsv(hsv);
+				this.screenConverterResult.setOrangeHudImage(orangeHudImage);
+				this.screenConverterResult.setYellowHudImage(yellowHudImage);
+				this.screenConverterResult.setBlueWhiteHudImage(blueWhiteHudImage);
+				this.screenConverterResult.setRedHudImage(redHudImage);
+				this.screenConverterResult.setBrightImage(brightImage);
+				this.screenConverterResult.notifyAll();
+			}
+		}
 
-        logger.info(this.getName() + " stopped");
-    }
+		logger.info(this.getName() + " stopped");
+	}
 
-    private void hsvToHudImages(Planar<GrayF32> hsv, GrayF32 orangeHudImage, GrayF32 yellowHudImage, GrayF32 blueWhiteHudImage, GrayF32 redHudImage, GrayF32 brightImage) {
-        for (int y = 0; y < hsv.height; y++) {
-            for (int x = 0; x < hsv.width; x++) {
-                float h = hsv.bands[0].unsafe_get(x, y);
-                float s = hsv.bands[1].unsafe_get(x, y);
-                float v = hsv.bands[2].unsafe_get(x, y);
+	private void hsvToHudImages(Planar<GrayF32> hsv, GrayF32 orangeHudImage, GrayF32 yellowHudImage, GrayF32 blueWhiteHudImage, GrayF32 redHudImage, GrayF32 brightImage) {
+		for (int y = 0; y < hsv.height; y++) {
+			for (int x = 0; x < hsv.width; x++) {
+				float h = hsv.bands[0].unsafe_get(x, y);
+				float s = hsv.bands[1].unsafe_get(x, y);
+				float v = hsv.bands[2].unsafe_get(x, y);
 
-                if ((s > 0.70f) && (v >= 0.50f) && (h >= 0.25f && h < 1.00f)) {
-                    // Orange
-                    orangeHudImage.unsafe_set(x, y, v);
-                } else {
-                    orangeHudImage.unsafe_set(x, y, 0);
-                }
+				if ((s > 0.70f) && (v >= 0.50f) && (h >= 0.25f && h < 1.00f)) {
+					// Orange
+					orangeHudImage.unsafe_set(x, y, v);
+				} else {
+					orangeHudImage.unsafe_set(x, y, 0);
+				}
 
-                if ((s > 0.30f) && (v >= 0.70f) && (h >= 0.25f && h < 1.00f)) {
-                    // Yellow
-                    yellowHudImage.unsafe_set(x, y, v);
-                } else {
-                    yellowHudImage.unsafe_set(x, y, 0);
-                }
+				if ((s > 0.9f) && (v >= 0.9f) && (h >= 0.5f && h < 1.0f)) {
+					// Yellow
+					yellowHudImage.unsafe_set(x, y, v);
+				} else {
+					yellowHudImage.unsafe_set(x, y, 0);
+				}
 
-                if (v >= 0.75f && s < 0.15f) {
-                    // White
-                    blueWhiteHudImage.unsafe_set(x, y, v * v);
-                } else if ((h > 3.14f && h < 3.84f) && s > 0.15f) {
-                    // Blue-white
-                    blueWhiteHudImage.unsafe_set(x, y, v * v);
-                } else {
-                    blueWhiteHudImage.unsafe_set(x, y, 0);
-                }
+				if (v >= 0.75f && s < 0.15f) {
+					// White
+					blueWhiteHudImage.unsafe_set(x, y, v * v);
+				} else if ((h > 3.14f && h < 3.84f) && s > 0.15f) {
+					// Blue-white
+					blueWhiteHudImage.unsafe_set(x, y, v * v);
+				} else {
+					blueWhiteHudImage.unsafe_set(x, y, 0);
+				}
 
-                if ((s > 0.80f) && (v >= 0.70f) && (h < 0.25f || h > 6.0f)) {
-                    // Red
-                    redHudImage.unsafe_set(x, y, v * v * v);
-                } else {
-                    redHudImage.unsafe_set(x, y, 0);
-                }
+				if ((s > 0.80f) && (v >= 0.70f) && (h < 0.25f || h > 6.0f)) {
+					// Red
+					redHudImage.unsafe_set(x, y, v * v * v);
+				} else {
+					redHudImage.unsafe_set(x, y, 0);
+				}
 
-                if (v >= 0.85f) {
-                    brightImage.unsafe_set(x, y, v);
-                } else {
-                    brightImage.unsafe_set(x, y, 0);
-                }
-            }
-        }
-    }
+				if (v >= 0.85f) {
+					brightImage.unsafe_set(x, y, v);
+				} else {
+					brightImage.unsafe_set(x, y, 0);
+				}
+			}
+		}
+	}
 
 }
--- src/main/java/borg/ed/cruisecontrol/sysmap/SysmapScannerResult.java
+++ src/main/java/borg/ed/cruisecontrol/sysmap/SysmapScannerResult.java
@@ -3,20 +3,43 @@ package borg.ed.cruisecontrol.sysmap;
 import java.util.ArrayList;
 import java.util.List;
 
+import boofcv.struct.image.GrayF32;
+import boofcv.struct.image.Planar;
+
 public class SysmapScannerResult {
 
-    private List<SysmapBody> bodies = new ArrayList<>();
+	private Planar<GrayF32> rgb = null;
+	private Planar<GrayF32> hsv = null;
+	private List<SysmapBody> bodies = new ArrayList<>();
 
-    public SysmapScannerResult(List<SysmapBody> bodies) {
-        this.setBodies(bodies);
-    }
+	public SysmapScannerResult(Planar<GrayF32> rgb, Planar<GrayF32> hsv, List<SysmapBody> bodies) {
+		this.setRgb(rgb);
+		this.setHsv(hsv);
+		this.setBodies(bodies);
+	}
 
-    public List<SysmapBody> getBodies() {
-        return bodies;
-    }
+	public Planar<GrayF32> getRgb() {
+		return rgb;
+	}
 
-    public void setBodies(List<SysmapBody> bodies) {
-        this.bodies = bodies;
-    }
+	public void setRgb(Planar<GrayF32> rgb) {
+		this.rgb = rgb;
+	}
+
+	public Planar<GrayF32> getHsv() {
+		return hsv;
+	}
+
+	public void setHsv(Planar<GrayF32> hsv) {
+		this.hsv = hsv;
+	}
+
+	public List<SysmapBody> getBodies() {
+		return bodies;
+	}
+
+	public void setBodies(List<SysmapBody> bodies) {
+		this.bodies = bodies;
+	}
 
 }
